8fec790ce58bf713d90a050c2d0e56a1eac50478
Kevin <kevle0215@gmail.com>
2024-08-21 21:18:22 +0900

    testing more messsage

--- api/send-email.js
+++ api/send-email.js
@@ -14,9 +14,9 @@ export default async function handler(req, res) {
         });
 
         const mailOptions = {
-            from: email,
+            from: process.env.EMAIL_USER,
             to: process.env.EMAIL_USER,
-            subject: `New message from ${name}`,
+            subject: `New message from ${name} ${email} ${reason}`,
             text: email + reason,
         };
 
